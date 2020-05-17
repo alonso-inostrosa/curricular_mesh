bea7110acaa7d1a03399d7811402024d1b92fbab
Alonso <alonso.inostrosa@gmail.com>
2020-05-16 21:13:32 -0400

    Adding Mesh
    Adding the curricular mesh class

---
+++ classes/curricular_mesh.py
@@ -0,0 +1,6 @@
+class CurricularMesh:
+    def __init__(self, career):
+        self.career = career
+        self.courses_by_semester = []
+        self.courses = []
+        self.courses_by_level = []
